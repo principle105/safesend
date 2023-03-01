ca7ff384ca138219975532d6584c3de7f45d0f05
principle105 <principle105@gmail.com>
2023-03-01 13:18:07 -0500

    added basic UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,73 @@
 <script lang="ts">
-    import { themeChange } from "theme-change";
     import { onMount } from "svelte";
+    import { themeChange } from "theme-change";
+    import FileInput from "./components/FileInput.svelte";
+    import IoMdSunny from "svelte-icons/io/IoMdSunny.svelte";
+    import IoMdMoon from "svelte-icons/io/IoMdMoon.svelte";
 
     onMount(() => {
         themeChange(false);
     });
+
+    let checked = localStorage.theme === "light";
 </script>
 
-<main class="m-auto w-5/6 sm:w-3/4 lg:w-[51.5%]">
-    <h1 class="text-4xl font-semibold dark:text-zinc-50">SafeSend</h1>
-    <input
-        type="checkbox"
-        data-toggle-theme="dark,light"
-        checked={localStorage.theme === "light"}
-    />
+<main class="mx-auto w-5/6 sm:w-3/4 lg:w-[51.5%] my-7">
+    <div class="flex justify-between mb-6">
+        <h1 class="text-4xl font-semibold dark:text-zinc-50">SafeSend</h1>
+        <input
+            type="checkbox"
+            id="checkbox"
+            class="hidden"
+            data-toggle-theme="dark,light"
+            bind:checked
+        />
+        <label class="w-6 h-6 text-zinc-400" for="checkbox">
+            {#if checked}
+                <IoMdSunny />
+            {:else}
+                <IoMdMoon />
+            {/if}
+        </label>
+    </div>
+    <div class="mb-6">
+        <label
+            for="password"
+            class="block mb-2 text-sm font-medium text-zinc-900 dark:text-white"
+        >
+            Password
+        </label>
+        <input
+            type="password"
+            id="password"
+            class="bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="•••••••••"
+            required
+        />
+    </div>
+    <div class="mb-6">
+        <label
+            for="confirm_password"
+            class="block mb-2 text-sm font-medium text-zinc-900 dark:text-white"
+        >
+            Confirm password
+        </label>
+        <input
+            type="password"
+            id="confirm_password"
+            class="bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="•••••••••"
+            required
+        />
+    </div>
+    <FileInput />
+    <div class="mt-4">
+        <button
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+            Encrypt
+        </button>
+    </div>
 </main>
 
 <style global>
---
+++ src/components/FileInput.svelte
@@ -0,0 +1,28 @@
+<div class="flex items-center justify-center w-full">
+    <label
+        for="dropzone-file"
+        class="flex flex-col items-center justify-center w-full h-64 border-2 border-zinc-300 border-dashed rounded-lg cursor-pointer bg-zinc-50 dark:hover:bg-bray-800 dark:bg-zinc-700 hover:bg-zinc-100 dark:border-zinc-600 dark:hover:border-zinc-500 dark:hover:bg-zinc-600"
+    >
+        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg
+                aria-hidden="true"
+                class="w-10 h-10 mb-3 text-zinc-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                /></svg
+            >
+            <p class="mb-2 text-sm text-zinc-500 dark:text-zinc-400">
+                <span class="font-semibold">Click to upload</span> or drag and drop
+            </p>
+            <p class="text-xs text-zinc-500 dark:text-zinc-400">Up to 1GB</p>
+        </div>
+        <input id="dropzone-file" type="file" class="hidden" />
+    </label>
+</div>
