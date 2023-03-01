011cfc17b0e60b1314239619c2757e2dc42f82c3
principle105 <principle105@gmail.com>
2023-03-01 08:18:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<div align="center">
+    <!-- <img src="" alt="Logo" width="240" height="240"> -->
+    <h1>Safe Send</h1>
+    <p>Safely share sensitive data across the Internet</p>
+</div>
---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { themeChange } from "theme-change";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        themeChange(false);
+    });
+</script>
+
+<main class="m-auto w-5/6 sm:w-3/4 lg:w-[51.5%]">
+    <h1 class="text-4xl font-semibold">SafeSend</h1>
+    <input
+        type="checkbox"
+        data-toggle-theme="dark,light"
+        checked={localStorage.theme === "light"}
+    />
+</main>
+
+<style global>
+    @tailwind utilities;
+    @tailwind components;
+    @tailwind base;
+</style>
